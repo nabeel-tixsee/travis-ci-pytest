c8100dc9f119c10a2a2a28f1b8d3ce6b170d7704
nabeel-tixsee <82917939+nabeel-tixsee@users.noreply.github.com>
2021-05-28 10:35:02 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-set -o errexit -o nounset
-echo "Setting git user name"
-git config user.name $GH_USER_NAME
-
-echo "Setting git user email"
-git config user.email $GH_USER_EMAIL
-
+./terraform-linux init
+./terraform-linux validate 
+./terraform-linux plan
+./terraform-linux apply -auto-approve
